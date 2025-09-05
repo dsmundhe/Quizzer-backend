c7dd6f1731c6a44bbee651ad01cf18f31485dfd3
dsmundhe <dipakmundhe2026@gmail.com>
2025-09-05 18:20:23 +0530

    error fix

--- index.js
+++ index.js
@@ -2,20 +2,28 @@
 const express = require("express");
 const serverless = require("serverless-http");
 require("dotenv").config();
-const { dbConnection } = require("../db/dbConnection");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
+// MongoDB connection
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+  }
+};
+
+dbConnection();
+
+// Create Express app
 const app = express();
 app.use(express.json());
-
-app.use(cors({
-  origin: "*",
-  credentials: true,
-}));
-
-// Connect to MongoDB
-dbConnection().then(() => console.log("MongoDB connected successfully"))
-              .catch((err) => console.error(err));
+app.use(cors({ origin: "*", credentials: true }));
 
 // Routes
 const userRouter = require("../routes/userRoutes");
@@ -24,9 +32,10 @@ const quizRouter = require("../routes/quizRoutes");
 app.use("/user", userRouter);
 app.use("/quiz", quizRouter);
 
+// Root route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Wrap Express app for serverless
+// Export for Vercel serverless
 module.exports.handler = serverless(app);
